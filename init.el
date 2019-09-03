34aae78593014001169d86eff5ebaa221d3ddb02
Zhiguang Li <muzili@gmail.com>
2019-09-03 17:02:20 +0800

    Fix the completion commit issue

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 
        :completion
        (company          ; the ultimate code completion backend
-        +tng +childframe)
+        +childframe)
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy               ; a search engine for love and life
