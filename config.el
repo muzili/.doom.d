7a8af7b3fe34a28c83dd2500ce4fd39841101e5d
Zhiguang Li <muzili@gmail.com>
2020-03-07 20:28:42 +0800

    Show trailing whitespace by default
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- config.el
+++ config.el
@@ -1160,3 +1160,6 @@ package is loaded.")
       (if (boundp 'ccls-initialization-options)
           (append ccls-initialization-options `(:cache (:directory ,(expand-file-name "~/.ccls-cache"))))
         `(:cache (:directory ,(expand-file-name "~/.ccls-cache")))))
+
+;; show trailing whitespace if possible
+(setq-default show-trailing-whitespace t)
